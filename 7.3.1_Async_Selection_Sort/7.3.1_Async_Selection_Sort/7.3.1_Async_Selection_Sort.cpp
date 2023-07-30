1aa4ee2cb2385f16766c2a6583b7d72fa4250f95
Ilia Surikov <surikovis89@gmail.com>
2023-07-30 14:34:53 +0300

    Initial commit

---
+++ 7.3.1_Async_Selection_Sort/7.3.1_Async_Selection_Sort/7.3.1_Async_Selection_Sort.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <future>
+#include <random>
+
+int find_min(int* arr, int size)
+{
+	int min = arr[0];
+	int min_j = 0;
+	for (int j = 0; j < size; ++j)
+	{
+		if (arr[j] < min)
+		{
+			min_j = j;
+			min = arr[j];
+		}
+	}
+	return min_j;
+}
+
+void selectionSort(int* arr, int size, std::promise<int*> sorted)
+{
+	if (size <= 1) { return; }
+	
+	for (int i = 0; i < size; ++i)
+	{
+		//Поиск минимального элемента реализовать в виде асинхронной задачи.
+		auto ft = std::async(std::launch::async, find_min, &arr[i], size - i);
+		
+		std::swap(arr[i + ft.get()], arr[i]);
+	}
+	sorted.set_value(arr);
+}
+
+void print_arr(int* arr, int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	const int size = 100;
+	int arr[size];
+	
+	int gens = size;
+	std::generate(arr, arr + size, [arr, gens]() mutable {
+		return --gens;
+		});
+
+	std::cout << "До сортировки:\n";
+	print_arr(arr, size);
+
+	//Результат должен возвращаться в основной поток через связку std::promise - std::future.
+	std::promise<int*> prom;
+	std::future<int*> ft = prom.get_future();
+	std::thread t1 (selectionSort, arr, size, std::move(prom));
+	t1.detach();
+
+	std::cout << "После сортировки:\n";
+	print_arr(ft.get(), size);
+}
---
+++ 7.3.2_Async_for_each/7.3.2_Async_for_each/7.3.2_Async_for_each.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <future>
+#include <algorithm>
+#include <barrier>
+#include <chrono>
+
+using namespace std::chrono_literals;
+std::mutex m_cout;
+using duration_ms = std::chrono::duration<double, std::milli>;
+
+template <typename It, typename T>
+void async_for_each(It begin, It end, void (*func)(T&))
+{	
+	static int cores = std::thread::hardware_concurrency();
+	//static int cores = 16;
+	//static std::barrier barrier(cores);
+	int size = std::distance(begin, end);
+	//определение макисмального размера блока, до которого продолжится рекурсивное деление массива исходя из кол-ва доступных ядер процессора
+	static int max_block_size = size / cores;
+	if (size <= max_block_size || max_block_size == 0)//max_block_size == 0 на случай малого размера массива контейнера, когда size < cores
+	{	
+		//для обеспечения честного кол-ва потоков равного кол-ву ядер можно добавить барьер, но это существенно замедляет функцию,
+		//т.к. создание новых потоков ресурсозатратный процесс, быстрее выполняется с частичным пересечением id потоков,
+		//когда отработавший поток отадется системой на выполнение следующей асинхронной задачи
+		//if(max_block_size != 0) 
+		//{barrier.arrive_and_wait();}//ожидание создания необходимого кол-ва потоков
+		//
+		std::for_each(begin, end, func);
+		m_cout.lock();
+		std::cout << "\nПоток " << std::this_thread::get_id() << "; кол-во обработанных элементов = " << size;
+		m_cout.unlock();
+	}
+	else
+	{	
+		It mid = begin;
+		std::advance(mid, size / 2);
+		auto ft1 = std::async(std::launch::async, async_for_each<It, T>, begin, mid, func);//запуск в новом потоке
+		
+		async_for_each(mid, end, func);//запуск в текущем потоке
+	}
+}
+
+void multiply(int &val)
+{
+	val *= 2;	
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+    
+	std::vector<int> v1(800, 4);
+	std::cout << "Входные данные: ";
+	for (const auto& el : v1)
+	{
+		std::cout << el << " ";
+	}
+	
+	auto start = std::chrono::high_resolution_clock::now();
+	async_for_each(v1.begin(), v1.end(), multiply);
+	auto finish = std::chrono::high_resolution_clock::now();
+	duration_ms delta = std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start);
+	std::cout << "\nВремя работы функции = " << delta.count() << " ms";
+
+	std::cout << "\nВыходные данные: ";
+	for (const auto& el : v1)
+	{
+		std::cout << el << " ";
+	}
+}
